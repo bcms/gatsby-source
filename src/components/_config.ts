6041d814ea12a730950719bda9d068b84cb260f5
Bbanez <banezprod@gmail.com>
2022-06-21 14:29:12 +0200

    Move config to Most

--- src/components/_config.ts
+++
@@ -1,5 +0,0 @@
-export class BCMSImageConfig {
-  static localeImageProcessing = false;
-  static cmsOrigin = process.env.GATSBY_BCMS_API_ORIGIN || '';
-  static publicApiKeyId = process.env.GATSBY_BCMS_API_PUBLIC_KEY || '';
-}
